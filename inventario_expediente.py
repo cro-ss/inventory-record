d032d81dd59516874ca73cdaea9960b692e3f038
Cross <vrostian99@gmail.com>
2026-08-30 22:48:16 -0500

    We add elif structure to avoid that the fist if overlook some dir and files from my folder

--- inventario_expediente.py
+++ inventario_expediente.py
@@ -16,18 +16,29 @@ print('Elementos en el expediente', contador)
 file_counter = 0
 directory_counter=0
 total_weight = 0
+no_available = 0
+others_counter=0
+
 for elemento in root.rglob('*'):
+   print(len(str(elemento)))
+   try:
     if elemento.is_file() == True:
         file_counter = file_counter + 1
         in_weight = elemento.stat().st_size
         total_weight += in_weight
+
+    elif elemento.is_dir():
+         directory_counter += 1
     else:
-        directory_counter += 1
+        others_counter += 1
+   except OSError:
+        no_available += 1
+
 print(f'El número de archivos es {file_counter}')
 print(f'El número de carpetas es {directory_counter}')
-print(f'La comprobación es {directory_counter + file_counter }')
+print(f'La comprobación es {directory_counter + file_counter + others_counter }')
 print(f'El peso de los archivos es {round((total_weight/1024)/1024,2) } MB')
-print(f'El peso de los archivos es {round(((total_weight/1024)/1024)/1000,2) } GB')
+print(f'El peso de los archivos es {round(((total_weight/1024)/1024)/1024,2) } GB')
 
 
 ###### Vamos a generar comando en este script de python.
@@ -35,8 +46,16 @@ print(f'El peso de los archivos es {round(((total_weight/1024)/1024)/1000,2) } G
 from datetime import datetime
 
 with open('inventario.txt','w', encoding='UTF-8') as reportes:
-    reportes.write(f'Invetario GAD_PARROQUIAL_TARQUI_2026, Documentos pertencientes al expediente de vialidad\n')
+    reportes.write(f'{root.name}, Documentos pertencientes al expediente de vialidad\n')
+    reportes.write(f'Generado: {datetime.now().strftime("%d/%b/%Y %H:%M:%S")}\n')
     reportes.write(f'El número de archivos es: {file_counter}\n')
     reportes.write(f'El número de carpetas es, {directory_counter}\n')
-    reportes.write(f'La comporbación es, {directory_counter+file_counter}\n')
+    reportes.write(f'El número archivos de otro formato es, {others_counter}\n')
+    reportes.write(f'La comprobación es, {directory_counter+file_counter+others_counter}\n')
+    reportes.write(f'El peso de los archivos es {round((total_weight/1024)/1024,2)} MB\n')
+    reportes.write(f'El peso total de los archivos es: {round(((total_weight/1024)/1024)/1024,2)}GB\n')
+    reportes.write(f'Los archivos no leidos son : {no_available}\n')
+
+
+
 
